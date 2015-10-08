0bc494547b70053daf9400ef73ca36cd26f89180
James Wang <jnwang@suse.com>
2015-10-08 16:16:08 +0800

    Add some reporter script to used by report how many testcases have been run.

---
+++ compare_qadb2/report1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for arch in `ls`; 
+do 
+    pushd $arch 2>&1 >/dev/null; 
+    echo ============================
+    echo $arch
+    echo ============================
+    echo
+    for host in `ls`; 
+    do 
+        
+        pushd $host 2>&1 >/dev/null; 
+        echo \* $host :
+        echo -----------------------------
+        ls -t | rev | uniq -s 20 -c | rev; 
+        echo
+        popd 2>&1 >/dev/null; 
+    done;     
+    popd 2>&1 >/dev/null;
+done | grep "3 *$\|^\*\|^xen\|=\|^x86"
