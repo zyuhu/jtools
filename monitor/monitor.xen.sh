7342e202684eaeb9db2900c858dc705ead2e35d9
James Wang <jnwang@suse.com>
2015-10-29 10:15:34 +0800

    Add a monitor for xen

---
+++ monitor/monitor.xen.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "------------------------------------------------------"
+echo "Beijing Servers:"
+echo "------------------------------------------------------"
+#210 
+for number in 114 211
+do
+    echo 147.2.207.$number:
+    sshpass -p susetesting ssh root@147.2.207.${number} screen -ls 
+    retval=$?
+    case ${retval} in
+        255)
+            ssh-keygen -R 147.2.207.${number} -f /home/jnwang/.ssh/known_hosts
+            ssh root@147.2.207.${number} screen -ls
+            ;;
+        6)
+            ssh root@147.2.207.${number} screen -ls
+            ;;
+    esac
+done
+
+echo "------------------------------------------------------"
+echo "Nuermberg Servers:"
+echo "------------------------------------------------------"
+
+for number in 54
+do
+    echo 10.162.2.$number:
+    sshpass -p susetesting ssh root@10.162.2.${number} screen -ls 
+    retval=$?
+    case ${retval} in
+        255)
+            ssh-keygen -R 10.162.2.${number} -f /home/jnwang/.ssh/known_hosts
+            ssh root@10.162.2.${number} screen -ls
+            ;;
+        6)
+            ssh root@10.162.2.${number} screen -ls
+            ;;
+    esac
+done
