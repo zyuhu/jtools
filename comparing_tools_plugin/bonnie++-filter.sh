bb6b53edd3fdabba4e76508962c8407d474dab97
James Wang <jnwang@suse.com>
2015-11-02 17:11:20 +0800

    Modify some comparing bonnie++

--- comparing_tools_plugin/bonnie++-filter.sh
+++ comparing_tools_plugin/bonnie++-filter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-awk '/mean/{if ($5 < -10) printf "%s **\n",$0; else print $0}'
+#awk ''
+awk '/^\[.Seq.*mean/{if ($6 < -1) printf "%s **\n",$0;}/Random.*Seeks.*mean/{if ($5 < -10)printf "%s**\n" $0;}/Random.*Create.*mean/{if ($13 < -10) printf"%s**\n" $0;}'
