b5f51c26deee5a3e56d0f9fd63a4ed4030c4204a
James Wang <jnwang@suse.com>
2015-11-23 17:43:29 +0800

    Testing run scheduler
            update weekly-report.sh

---
+++ schedule/scheduler.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+int machines = 3;
+unsigned int sum = -1;
+unsigned int sum1 = -1;
+
+int i1;
+int i2;
+int i3;
+int i4;
+int i5;
+int i6;
+int i7;
+int i8;
+int i9;
+int i10;
+int i11;
+int i12;
+int i13;
+int i14;
+int i15;
+int i16;
+int i17;
+int i18;
+int i19;
+int i20;
+int i21;
+int i22;
+int i23;
+int i24;
+int i25;
+int i26;
+int i27;
+int i28;
+int i29;
+int i30;
+int i31;
+int i32;
+int i33;
+int i34;
+int i35;
+int i36;
+int i37;
+int i38;
+int i39;
+int i40;
+int i41;
+int i42;
+int i43;
+int i44;
+int i45;
+int i46;
+int i47;
+int i48;
+
+
+int array[][3]={
+2231,4048,0,
+2473,4391,3415,
+2303,4072,3094,
+2221,4092,0,
+2252,4110,0,
+2493,4383,3505,
+2293,4082,3263,
+2247,4119,0,
+4437,4448,4427,
+4418,4493,4441,
+4417,4431,4421,
+4417,4432,4502,
+4444,4451,4431,
+4418,4421,4420,
+4418,4429,4420,
+4418,4447,4503,
+891,576,999,
+205,80,167,
+10227,16790,6254,
+2415,1394,1395,
+2415,2414,1635,
+3237,2060,0,
+3305,2081,2726,
+3190,2064,2555,
+3177,2047,0,
+3166,5662,0,
+2360,2178,9459,
+2348,1879,8354,
+2452,1886,0,
+7046,15648,0,
+8294,15606,10562,
+8028,15473,10861,
+6864,15372,10660,
+49,46,78,
+2495,5434,1964,
+3271,5883,3763,
+3141,5563,3763,
+2705,5538,3726,
+31892,31893,31897,
+454,798,3094,
+658,809,1972,
+647,763,1901,
+444,795,2932,
+392,327,444,
+389,362,442,
+379,319,437,
+383,316,420,
+238,408,271,
+};
+
+int main(int argc,char **argv)
+{
+sum = array[0][i1]+array[1][i2]+array[2][i3]+array[3][i4]+array[4][i5]+        array[5][i6]+array[6][i7]+array[7][i8]+array[8][i9]+array[9][i10]+array[10][i11]+array[11][i12]+array[12][i13]+array[13][i14]+         array[14][i15]+array[15][i16]+array[16][i17]+array[17][i18]+array[18][i19]+array[19][i20]+array[20][i21]+array[21][i22]+array[22][i23]+array[23][i24]+array[24][i25]+array[25][i26]+array[26][i27]+array[27][i28]+array[28][i29]+array[29][i30]+array[30][i31]+array[31][i32]+array[32][i33]+array[33][i34]+array[34][i35]+array[35][i36]+array[36][i37]+array[37][i38]+array[38][i39]+array[39][i40]+array[40][i41]+array[41][i42]+array[42][i43]+array[43][i44]+array[44][i45]+array[45][i46]+array[46][i47]+array[47][i48];
+
+for (i1=0;i1<machines;i1++) {
+for (i2=0;i2<machines;i2++) {
+    for (i3=0;i3<machines;i3++) {
+        for (i4=0;i4<machines;i4++) {
+            for (i5=0;i5<machines;i5++) {
+                for (i6=0;i6<machines;i6++) {
+                    for (i7=0;i7<machines;i7++) {
+                        for (i8=0;i8<machines;i8++) {
+                            for (i9=0;i9<machines;i9++) {
+                                for (i10=0;i10<machines;i10++) {
+                                    for (i11=0;i11<machines;i11++) {
+                                        for (i12=0;i12<machines;i12++) {
+                                            for (i13=0;i13<machines;i13++) {
+                                                for (i14=0;i14<machines;i14++) {
+                                                    for (i15=0;i15<machines;i15++) {
+                                                        for (i16=0;i16<machines;i16++) {
+                                                            for (i17=0;i17<machines;i17++) {
+                                                                for (i18=0;i18<machines;i18++) {
+                                                                    for (i19=0;i19<machines;i19++) {
+                                                                        for (i20=0;i20<machines;i20++) {
+                                                                            for (i21=0;i21<machines;i21++) {
+                                                                                for (i22=0;i22<machines;i22++) {
+                                                                                    for (i23=0;i23<machines;i23++) {
+                                                                                        for (i24=0;i24<machines;i24++) {
+                                                                                            for (i25=0;i25<machines;i25++) {
+                                                                                                for (i26=0;i26<machines;i26++) {
+                                                                                                    for (i27=0;i27<machines;i27++) {
+                                                                                                        for (i28=0;i28<machines;i28++) {
+                                                                                                            for (i29=0;i29<machines;i29++) {
+                                                                                                                for (i30=0;i30<machines;i30++) {
+                                                                                                                    for (i31=0;i31<machines;i31++) {
+                                                                                                                        for (i32=0;i32<machines;i32++) {
+                                                                                                                            for (i33=0;i33<machines;i33++) {
+                                                                                                                                for (i34=0;i34<machines;i34++) {
+                                                                                                                                    for (i35=0;i35<machines;i35++) {
+                                                                                                                                        for (i36=0;i36<machines;i36++) {
+                                                                                                                                            for (i37=0;i37<machines;i37++) {
+                                                                                                                                                for (i38=0;i38<machines;i38++) {
+                                                                                                                                                    for (i39=0;i39<machines;i39++) {
+                                                                                                                                                        for (i40=0;i40<machines;i40++) {
+                                                                                                                                                            for (i41=0;i41<machines;i41++) {
+                                                                                                                                                                for (i42=0;i42<machines;i42++) {
+                                                                                                                                                                    for (i43=0;i43<machines;i43++) {
+                                                                                                                                                                        for (i44=0;i44<machines;i44++) {
+                                                                                                                                                                            for (i45=0;i45<machines;i45++) {
+                                                                                                                                                                                for (i46=0;i46<machines;i46++) {
+                                                                                                                                                                                    for (i47=0;i47<machines;i47++) {
+                                                                                                                                                                                        for (i48=0;i48<machines;i48++) {
+                                                               
+                                                                 if( array[0][i1] == 0 || array[1][i2] == 0 || array[2][i3] == 0 || array[3][i4] == 0 || array[4][i5] == 0 || array[5][i6] == 0 || array[6][i7] == 0 || array[7][i8] == 0 || array[8][i9] == 0 || array[9][i10] == 0 || array[10][i11] == 0 || array[11][i12] == 0 || array[12][i13] == 0 || array[13][i14] == 0 || array[14][i15] == 0 || array[15][i16] == 0 || array[16][i17] == 0 || array[17][i18] == 0 || array[18][i19] == 0 || array[19][i20] == 0 || array[20][i21] == 0 || array[21][i22] == 0 || array[22][i23] == 0 || array[23][i24] == 0 || array[24][i25] == 0 || array[25][i26] == 0 || array[26][i27] == 0 || array[27][i28] == 0 || array[28][i29] == 0 || array[29][i30] == 0 || array[30][i31] == 0 || array[31][i32] == 0 || array[32][i33] == 0 || array[33][i34] == 0 || array[34][i35] == 0 || array[35][i36] == 0 || array[36][i37] == 0 || array[37][i38] == 0 || array[38][i39] == 0 || array[39][i40] == 0 || array[40][i41] == 0 || array[41][i42] == 0 || array[42][i43] == 0 || array[43][i44] == 0 || array[44][i45] == 0 || array[45][i46] == 0 || array[46][i47] == 0 || array[47][i48] == 0 )                                                                                                                               {
+                                                                 
+                                                                    continue;
+                                                                 }
+                                                                                                                                                                                              sum1 = array[0][i1]+array[1][i2]+array[2][i3]+array[3][i4]+array[4][i5]+array[5][i6]+array[6][i7]+array[7][i8]+array[8][i9]+array[9][i10]+array[10][i11]+array[11][i12]+array[12][i13]+array[13][i14]+array[14][i15]+array[15][i16]+array[16][i17]+array[17][i18]+array[18][i19]+array[19][i20]+array[20][i21]+array[21][i22]+array[22][i23]+array[23][i24]+array[24][i25]+array[25][i26]+array[26][i27]+array[27][i28]+array[28][i29]+array[29][i30]+array[30][i31]+array[31][i32]+array[32][i33]+array[33][i34]+array[34][i35]+array[35][i36]+array[36][i37]+array[37][i38]+array[38][i39]+array[39][i40]+array[40][i41]+array[41][i42]+array[42][i43]+array[43][i44]+array[44][i45]+array[45][i46]+array[46][i47]+array[47][i48];
+                                                                                                                                                                                              if (sum > sum1) {
+                                                                                                                                                                                                sum = sum1;
+                                                                                                                                                                                                printf("<result %d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d</result>\n",i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15,i16,i17,i18,i19,i20,i21,i22,i23,i24,i25,i26,i27,i28,i29,i30,i31,i32,i33,i34,i35,i36,i37,i38,i39,i40,i41,i42,i43,i44,i45,i46,i47,i48);
+                                                                                                                                                                                              }
+                                                                                                                                                                                            }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+
+    return 0;
+}
