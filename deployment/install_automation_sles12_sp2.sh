42890916fcd7b2d02eb3ce0cd6cb010a0376da70
James Wang <jnwang@suse.com>
2016-07-06 14:59:36 +0800

    remove hostname hardcode

--- deployment/install_automation_sles12_sp2.sh
+++ deployment/install_automation_sles12_sp2.sh
@@ -2,9 +2,11 @@
 
 zypper rr qa-ibs
 zypper ar http://dist.nue.suse.com/ibs/QA:/Head/SLE-12-SP2/ qa-ibs
+zypper ar http://147.2.207.1/dist/install/SLP/SLE-12-SP2-SDK-LATEST/x86_64/DVD1/ qa-sdk
 zypper ref
 
 zypper -n install qa_testset_automation
+zypper -n install git
 
 
 zypper -n rm snapper-zypp-plugin
@@ -16,7 +18,3 @@ zypper install ca-certificates-suse
 
 zypper rm qa_hamsta
 
-
-t=`hostname`
-echo "${t}" > /etc/hostname
-hostname -F /etc/hostname
